9d0c98f0d0c39d714634e49bfad5a588c7d70fcf
Abderraoufzekkour <abderraoufzekkour05@gmail.com>
2026-07-24 19:32:49 +0000

    feat: add Scaffolder, TechDocs, Org, Kubernetes and ArgoCD plugins to frontend

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,37 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import searchPlugin from '@backstage/plugin-search/alpha';
+import scaffolderPlugin from '@backstage/plugin-scaffolder/alpha';
+import userSettingsPlugin from '@backstage/plugin-user-settings/alpha';
+import techdocsPlugin from '@backstage/plugin-techdocs/alpha';
+import orgPlugin from '@backstage/plugin-org/alpha';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props => (
+      <SignInPage {...props} providers={['guest']} />
+    ),
+  },
+});
 
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [
+    catalogPlugin,
+    searchPlugin,
+    scaffolderPlugin,
+    userSettingsPlugin,
+    techdocsPlugin,
+    orgPlugin,
+    kubernetesPlugin,
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
