8b7ebb243c469fc6511595ed49aa3dc2c96fda45
Abderraoufzekkour <abderraoufzekkour05@gmail.com>
2026-08-01 22:56:02 +0000

    feat: add catalog-import plugin to new frontend API

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -7,16 +7,30 @@ import userSettingsPlugin from '@backstage/plugin-user-settings/alpha';
 import techdocsPlugin from '@backstage/plugin-techdocs/alpha';
 import orgPlugin from '@backstage/plugin-org/alpha';
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
+import catalogImportPlugin from '@backstage/plugin-catalog-import/alpha';
 import { navModule } from './modules/nav';
 
 import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import { SignInPage } from '@backstage/core-components';
 import { createFrontendModule } from '@backstage/frontend-plugin-api';
+import { oidcAuthApiRef } from '@backstage/core-plugin-api';
 
 const signInPage = SignInPageBlueprint.make({
   params: {
     loader: async () => props => (
-      <SignInPage {...props} providers={['guest']} />
+      <SignInPage
+        {...props}
+        auto
+        providers={[
+          'guest',
+          {
+            id: 'oidc',
+            title: 'Keycloak',
+            message: 'Sign in using Keycloak',
+            apiRef: oidcAuthApiRef,
+          },
+        ]}
+      />
     ),
   },
 });
@@ -30,6 +44,7 @@ export default createApp({
     techdocsPlugin,
     orgPlugin,
     kubernetesPlugin,
+    catalogImportPlugin,
     navModule,
     createFrontendModule({
       pluginId: 'app',
